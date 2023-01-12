d811cde820461fba1874101994499b844da4e26e
Boromant <anthony.michallet42@gmail.com>
2023-01-12 18:49:53 +0100

    Affichage, modif, suppression de Users

--- src/main/java/com/group/byke/repositories/EntityUsersRepository.java
+++ src/main/java/com/group/byke/repositories/EntityUsersRepository.java
@@ -1,7 +1,11 @@
 package com.group.byke.repositories;
 
 import com.group.byke.domains.EntityUsers;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +20,15 @@ public interface EntityUsersRepository extends JpaRepository<EntityUsers, Intege
 
    @Query("Select id from EntityUsers where nomUtil = :nomUtil")
    int getIdByNomUtil(@Param("nomUtil") String nomUtil);
+
+   @Modifying
+   @Transactional
+   @Query("UPDATE EntityUsers SET nomUtil= :nomUtil ," +
+            " motPasse= :motPasse ," +
+            " role= :role" +
+            " WHERE numUtil = :numUtil")
+   int updateUser(@Param("numUtil") int id,
+                  @Param("nomUtil") String nomUtil,
+                  @Param("motPasse") String motPasse,
+                  @Param("role") String role);
 }
